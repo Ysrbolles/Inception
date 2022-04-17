c887e01fb6a62fcbadb012137f79d35740d53ee2
ybolles <ysrbolles@gmail.com>
2022-04-17 02:23:18 +0000

    🔥 Create Nginx Dockerfile

--- srcs/requirements/mariadb/tools/create_db.sql
+++ srcs/requirements/mariadb/tools/create_db.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE ysrbolles;
 CREATE DATABASE ybolles;
-CREATE USER 'ybolles'@'localhost';
+CREATE USER 'ybolles'@'%';
 GRANT ALL PRIVILEGES ON ybolles.* to 'wpuser'@'localhost' IDENTIFIED BY 'ybolles';
+GRANT ALL PRIVILEGES ON ybolles.* to 'wpuser'@'%' IDENTIFIED BY 'ybolles';
 FLUSH PRIVILEGES;
