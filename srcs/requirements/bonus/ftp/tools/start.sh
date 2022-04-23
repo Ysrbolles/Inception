ceb5c70b4b2a8f381390cb9ee1d83380ae54cbd4
ybolles <ysrbolles@gmail.com>
2022-04-23 06:52:22 +0000

    🚑 Trying to fix FTP shell errors

--- srcs/requirements/bonus/ftp/tools/start.sh
+++ srcs/requirements/bonus/ftp/tools/start.sh
@@ -1,11 +1,22 @@
-# #!/bin/sh
+#!/bin/bsh
 
-# adduser $FTPS_USER
-# echo "ybolles:ybolles" | chpasswd
+service vsftpd start
+adduser -h
+# echo "$FTPS_USER:$FTPS_USER"
+# echo "$FTPS_USER:$FTPS_USER"| chpasswd
 # mkdir -p /home/ybolles/ftp
-# chown ybolles:ybolles /home/ybolles/ftp
+# ls -la /home/ybolles/ftp
+# chown $FTPS_USER:$FTPS_USER /home/ybolles/ftp
+# mkdir -p /home/ybolles/ftp/files
+# chown $FTPS_USER:$FTPS_USER /home/ybolles/ftp/files
 # chmod a-w /home/ybolles/ftp
-# echo "ybolles" | tee -a /etc/vsftpd.userlist
+# echo "$FTPS_USER" | tee -a /etc/vsftpd.userlist
+# rm -rf /etc/vsftpd.conf
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem -subj "/C=MA/ST=KHOURIBGA/L=Kkouribga/O=1337/OU=My Department/CN=localhost:21"
+# echo "$FTPS_USER hello #################################################################"
+# cp vsftpd.conf /etc/vsftpd.conf
 # service vsftpd restart
 
-# exec vsftpd /etc/vsftpd.conf
+
+
+exec vsftpd /etc/vsftpd.conf
