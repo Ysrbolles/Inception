4e355ab10613f14085fc78721bc92b6d9772925c
ybolles <ysrbolles@gmail.com>
2022-04-23 23:12:51 +0000

    🐛 Fix FTP server errors

--- srcs/requirements/bonus/ftp/tools/start.sh
+++ srcs/requirements/bonus/ftp/tools/start.sh
@@ -9,12 +9,8 @@ mkdir -p /home/ybolles/ftp/files
 chown $FTPS_USER:$FTPS_USER /home/ybolles/ftp/files
 chmod a-w /home/ybolles/ftp
 echo "$FTPS_USER" | tee -a /etc/vsftpd.userlist
-# rm -rf /etc/vsftpd.conf
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem -subj "/C=MA/ST=KHOURIBGA/L=Kkouribga/O=1337/OU=My Department/CN=localhost:21"
-# echo "$FTPS_USER hello #################################################################"
 cp vsftpd.conf /etc/vsftpd.conf
-service vsftpd restart
-
 
 
 exec vsftpd /etc/vsftpd.conf
